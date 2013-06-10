3a51bd1395ccffde946ee8168f2fb64b21ab5ad8
iamluc <luc@vieillescazes.net>
2013-06-10 17:56:24 +0200

    Fix #6 : SerializerHandler loaded only if jms_serializer is available

--- GloomyPagerBundle.php
+++ GloomyPagerBundle.php
@@ -3,7 +3,15 @@
 namespace Gloomy\PagerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Gloomy\PagerBundle\DependencyInjection\ContainerBuilder\SerializerCompilerPass;
 
 class GloomyPagerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new SerializerCompilerPass());
+    }
 }
